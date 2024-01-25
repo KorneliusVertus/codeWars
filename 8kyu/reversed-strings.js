309b3ebf9b7480df0a268e4e5e1149c613feb697
KorneliusVertus <aleksei.kornelius@gmail.com>
2024-01-25 16:56:02 +0300

    return-negative

--- 8kyu/reversed-strings.js
+++ 8kyu/reversed-strings.js
@@ -6,7 +6,7 @@
 // SOLUTION
 
 function solution(str){
-    return str.split('').reverse().join(''); 
+    return str.split('').reverse().join(''); // split - из strings in array; reverse - перевернуть строку; join - из array in stings
 }
 
 const result = solution('Alex');
